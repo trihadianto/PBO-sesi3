6f63da4087c2e8a3012d08d1407342e90ee7305b
tri hadianto <114473435+trihadianto@users.noreply.github.com>
2024-03-06 05:33:45 -0800

    Update App.java

--- App.java
+++ App.java
@@ -2,9 +2,25 @@ public class App {
     public static void main(String[] args) {
         Persegi persegi = new Persegi(2.0);
         persegi.getInfo(); 
+        System.out.println();
+
+        Lingkaran lingkaran = new Lingkaran(7.0);
+        lingkaran.getInfo();
+        System.out.println();
         
-        Segitiga segitiga = new segitiga(alas 2, tinggi 5);
-        segitiga.getInfo();
+        SamaSisi samasisi = new SamaSisi(5.0, 4.0);
+        samasisi.getInfo();
+        System.out.println();
+
+        SamaKaki Samakaki = new SamaKaki(10.0, 3.0);
+        Samakaki.getInfo();
+        System.out.println();
+
+        SikuSiku Sikusiku = new SikuSiku(10.0, 5.0);
+        Sikusiku.getInfo();
+        System.out.println();
+
+        Sembarang Sembarang = new Sembarang(5.0, 5.0, 4.0);
+        Sembarang.getInfo();
     }
 }
-
